bb7316bdef96b532190001ffee912a5e45fb15ec
Shad MOrdre <shadmordre@gmail.com>
2021-10-16 12:06:32 -0700

    Add files via upload

---
+++ schems/lib_ecology_schem_tree_mamaku_01.lua
@@ -0,0 +1,33 @@
+
+
+--Mamaku Tree 01
+	local N = gal.N
+
+	
+	return {
+		size = {x = 1,y = 10,z = 1},
+		data = {
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["L0m00"]
+		},
+		yslice_prob = {
+			{ypos = 0,prob = 254},
+			{ypos = 1,prob = 254},
+			{ypos = 2,prob = 126},
+			{ypos = 3,prob = 254},
+			{ypos = 4,prob = 254},
+			{ypos = 5,prob = 254},
+			{ypos = 6,prob = 254},
+			{ypos = 7,prob = 254},
+			{ypos = 8,prob = 254},
+			{ypos = 9,prob = 254}
+		}
+	}
