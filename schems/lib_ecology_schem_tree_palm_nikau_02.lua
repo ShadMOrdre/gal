e3beb0f5ddbe5cc008c34d2e568888dadab6e862
Shad MOrdre <shadmordre@gmail.com>
2021-10-16 12:09:45 -0700

    Add files via upload

---
+++ schems/lib_ecology_schem_tree_palm_nikau_02.lua
@@ -0,0 +1,43 @@
+
+
+--Nikau Palm Tree 02
+	local N = gal.N
+
+	
+	return {
+		size = {x = 1,y = 15,z = 1},
+		data = {
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["L0m00"]
+		},
+		yslice_prob = {
+			{ypos = 0,prob = 254},
+			{ypos = 1,prob = 254},
+			{ypos = 2,prob = 126},
+			{ypos = 3,prob = 254},
+			{ypos = 4,prob = 254},
+			{ypos = 5,prob = 254},
+			{ypos = 6,prob = 254},
+			{ypos = 7,prob = 254},
+			{ypos = 8,prob = 254},
+			{ypos = 9,prob = 254},
+			{ypos = 10,prob = 254},
+			{ypos = 11,prob = 254},
+			{ypos = 12,prob = 254},
+			{ypos = 13,prob = 254},
+			{ypos = 14,prob = 254}
+		}
+	}
