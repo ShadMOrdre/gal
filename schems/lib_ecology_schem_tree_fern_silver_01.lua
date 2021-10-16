364f3c3f45b0fa82640251db052ca1eae7e0baeb
Shad MOrdre <shadmordre@gmail.com>
2021-10-16 12:04:10 -0700

    Add files via upload

---
+++ schems/lib_ecology_schem_tree_fern_silver_01.lua
@@ -0,0 +1,23 @@
+
+
+--Silver Fern Tree 01
+	local N = gal.N
+
+	
+	return {
+		size = {x = 1,y = 5,z = 1},
+		data = {
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["T0m00"],
+			N["L0m00"],
+		},
+		yslice_prob = {
+			{ypos = 0,prob = 254},
+			{ypos = 1,prob = 254},
+			{ypos = 2,prob = 126},
+			{ypos = 3,prob = 254},
+			{ypos = 4,prob = 254}
+		}
+	}
